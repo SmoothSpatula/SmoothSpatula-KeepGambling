a1e7cd4ace8620ff735f2e374336218399ed15a1
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-10-31 11:06:49 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+-- Keep Gambling v1.0.0
+-- SmoothSpatula
+
+log.info("Successfully loaded ".._ENV["!guid"]..".")
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto() -- Loading the toolkit 
+
+local set_shrines = function ()
+    local shrines, exist = Instance.find_all(gm.constants.pInteractableShrine)
+    if exist then
+        for _, shrine in ipairs(shrines) do
+            shrine.charges = 1000000
+        end
+    end
+end
+
+local initialize = function()
+    Callback.add("onStageStart", "KeepGambling-onStageStart", set_shrines)
+end
+Initialize(initialize)
